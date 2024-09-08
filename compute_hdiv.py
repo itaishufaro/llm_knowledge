ed2af2420570dee8599848fefce095b552c9a815
idan <idankash@gmail.com>
2024-09-08 13:04:05 +0300

    adapt format

--- compute_hdiv.py
+++ compute_hdiv.py
@@ -2,27 +2,27 @@ import pandas as pd
 import numpy as np
 RESULTS_PATH = "results.csv"
 
-df = pd.read_csv(RESULTS_PATH, sep=",")
+df = pd.read_csv(RESULTS_PATH, sep=", ")
 
 def h_diveregence(model1, model2, ref_model, subject):
     def h_entropy(model, ref_model):
-        loss_column = df.columns.get_loc('mean_loss')
-        model_loss = df[(df['hf_model_path'] == model) & (df['mmlu_subject'] == subject)].values[0, loss_column]
+        subject_column = df.columns.get_loc(subject)
+        model_loss = df[df["hf_model_path"] == model].values[0, subject_column]
         if ref_model == 'random':
             ref_loss = 0.75
         else:
-            ref_loss = df[(df['hf_model_path'] == ref_model) & (df['mmlu_subject'] == subject)].values[0, loss_column]
+            ref_loss = df[df["hf_model_path"] == ref_model].values[0, subject_column]
         return min(np.sum(model_loss), np.sum(ref_loss))
 
     def h_entropy_mixed(ref_model):
-        loss_column = df.columns.get_loc('mean_loss')
-        model_loss1 = df[(df['hf_model_path'] == model1) & (df['mmlu_subject'] == subject)].values[0, loss_column]
-        model_loss2 = df[(df['hf_model_path'] == model2) & (df['mmlu_subject'] == subject)].values[0, loss_column]
+        subject_column = df.columns.get_loc(subject)
+        model_loss1 = df[df["hf_model_path"] == model1].values[0, subject_column]
+        model_loss2 = df[df["hf_model_path"] == model2].values[0, subject_column]
         mixed_loss = 0.5*(model_loss1+model_loss2)
         if ref_model == 'random':
             ref_loss = 0.75
         else:
-            ref_loss = df[(df['hf_model_path'] == ref_model) & (df['mmlu_subject'] == subject)].values[0, loss_column]
+            ref_loss = df[df["hf_model_path"] == ref_model].values[0, subject_column]
         return min(np.sum(mixed_loss), np.sum(ref_loss))
     
     return h_entropy_mixed(ref_model) - min(h_entropy(model1, ref_model), h_entropy(model2, ref_model))
--- full_exp.py
+++ full_exp.py
@@ -4,10 +4,10 @@ import compute_hdiv as ch
 
 
 if __name__ == '__main__':
-    df = pd.read_csv(ch.RESULTS_PATH, sep=",")
-    subjects_list = df['mmlu_subject'].unique()
+    df = pd.read_csv(ch.RESULTS_PATH, sep=", ")
+    subjects_list = df.columns[1:]
     model1 = "meta-llama/Meta-Llama-3-8B-Instruct"
-    model2 = "mistralai/Mistral-7B-Instruct-v0.3"
+    model2 = "mistralai/Mistral-7B-Instruct-v0.1"
     ref_model = "random"
 
     # Print the 5 subjects with the highest h-divergence and the 5 subjects with the lowest h-divergence
@@ -16,9 +16,11 @@ if __name__ == '__main__':
         h_divergences.append(ch.h_diveregence(model1, model2, ref_model, subject))
     h_divergences = np.array(h_divergences)
     sorted_indices = np.argsort(h_divergences)
+    print('\n=============================\n')
     print("Subjects with the lowest h-divergence:")
     for i in range(5):
         print(subjects_list[sorted_indices[i]], h_divergences[sorted_indices[i]])
+    print('\n=============================\n')
     print("Subjects with the highest h-divergence:")
     for i in range(1, 6):
         print(subjects_list[sorted_indices[-i]], h_divergences[sorted_indices[-i]])
